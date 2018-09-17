f29616299216a27572db00974c051c43d8249b30
Guillaume Gojard <ggojard@gmail.com>
2018-09-17 22:36:27 +0200

    update

--- app.py
+++ app.py
@@ -96,13 +96,18 @@ def get_actionable_ranking():
     if users:
         for user in users:
             tag = 'appinventor_user_actionable_scores_' + user
-            board += '<br>' + tag
+            #board += '<br>' + tag
             nb_play = 0
             sum_play = 0
             average = 0.00
             value = TinyWebDB.query.filter_by(tag=tag).first().value.replace("[", "").replace("]", "").split(',');
-            if value:
-                board += '<br>  --  ' + value.join(",")
+            nb_play = len(value)
+            for v in value:
+                sum_play = sum_play + int(v)
+            nb_play = len(value)
+            average = format(sum_play/nb_play, '.2f')
+            board += '<br> average: ' + average
+            #return jsonify(['VALUE', 'nb', nb_play, 'sum', sum_play, 'average', average])
 
     return board
 
