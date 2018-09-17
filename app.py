dd07275cf4d786a8c3e5af71bf427292e93d2156
Guit <ggojard@gmail.com>
2018-09-17 12:01:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -84,21 +84,20 @@ def get_averages():
    
     for user in users:
         tag = 'appinventor_user_actionable_scores_' + user       
-        board += '<br>' + tag
+        # board += '<br>' + tag
         nb_play = 0
         sum_play = 0
         average = 0.00
         value = TinyWebDB.query.filter_by(tag=tag).first().value #.replace("[", "").replace("]", "").split(',');
         if value:
-             board += '<br>' + value      
-         #   nb_play = len(value)
-         #   for v in value:
-         #       sum_play = sum_play + int(v)
-         #   nb_play = len(value)
-         #   average = format(sum_play/nb_play, '.2f')
+         #    board += '<br>' + value      
+             nb_play = len(value)
+             for v in value:
+                    sum_play = sum_play + int(v)
+             nb_play = len(value)
+             average = format(sum_play/nb_play, '.2f')
          #   board += user + ': ' + ['VALUE', 'nb', nb_play, 'sum', sum_play, 'average', average]) + '<\br>'
     return board
-
 
 @app.route('/deleteentry')
 def delete_entry():
