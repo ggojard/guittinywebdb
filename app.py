80bc09a97d792be351ef6d70437b124751629f50
Guit <ggojard@gmail.com>
2018-09-26 18:13:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def store_a_score():
 def actionable_create_user():
     user = request.form['user']
     tag = 'appinventor_user_actionable_scores_' + user
-    empty_scores = []
+    empty_scores = '[]'
     store_a_value(tag, empty_scores)
     tag = 'appinventor_user_actionable_scores_ranking'
     return add_item_to_tag_value(tag, '"'+user+'"')
